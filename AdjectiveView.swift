9da2a3142472824bb106c22406b556927f7c4dfa
SaraGrabowski <sgrabowski3149@stu.d214.org>
2023-04-06 14:20:51 -0500

    added adj and verb views

---
+++ AdjectiveView.swift
@@ -0,0 +1,14 @@
+//
+//  AdjectiveView.swift
+//  GitHubPractice2
+//
+//  Created by Sara Grabowski on 4/6/23.
+//
+
+import SwiftUI
+
+struct AdjectiveView: View {
+    var body: some View {
+        Text("Delete")
+    }
+}
