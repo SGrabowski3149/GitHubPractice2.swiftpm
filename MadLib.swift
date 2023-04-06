227bf70f002975a4ae66df5454c3bfcde0ca5566
Tanya Cervantes <tcervantes3491@stu.d214.org>
2023-04-06 14:27:48 -0500

    added madlib struct

---
+++ MadLib.swift
@@ -0,0 +1,17 @@
+//
+//  MadLib.swift
+//  GitHubPractice2
+//
+//  Created by Tanya Cervantes on 4/6/23.
+//
+
+import SwiftUI
+
+struct MadLib {
+    
+    var noun: String = ""
+    var verb: String = ""
+    var adjective:  String =  ""
+    
+
+}
