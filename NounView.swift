83a679dda9bd3728ab44dfa7acab6003c67f876b
Tanya Cervantes <tcervantes3491@stu.d214.org>
2023-04-06 14:06:41 -0500

    added noun view

---
+++ NounView.swift
@@ -0,0 +1,17 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Tanya Cervantes on 4/6/23.
+//
+
+import SwiftUI
+
+struct NounView: View {
+    var body: some View {
+        
+        NavigationLink("Verb View"){
+            VerbView()
+        }
+    }
+}
