b54babc0ef5c64bfb63868b4192d64e3f45743d1
SaraGrabowski <sgrabowski3149@stu.d214.org>
2023-04-06 14:15:36 -0500

    added noun view

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            
+            NavigationLink ("Noun View") {
+                NounView()
+            }
         }
     }
 }
--- NounView.swift
+++ NounView.swift
@@ -2,16 +2,11 @@
 //  NounView.swift
 //  GitHubPractice2
 //
-//  Created by Tanya Cervantes on 4/6/23.
+//  Created by Sara Grabowski on 4/6/23.
 //
 
 import SwiftUI
 
 struct NounView: View {
-    var body: some View {
-        
-        NavigationLink("Verb View"){
-            VerbView()
-        }
-    }
+    
 }
